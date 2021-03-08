90ceefecb9c414852b970a53a90a814e2dee3cbc
Diamond Cat <uyuuki0919@icloud.com>
2021-03-09 07:39:16 +0900

    update PATCH

--- run.sh
+++ run.sh
@@ -8,14 +8,12 @@ cd template
 git switch -C web -f 8b23b3ff1879ca0442775a0ebf1defab72bbb79f
 git clean -df
 
-[ ! -f ../web.patch ] || git am ../web.patch
+[ ! -f ../web.patch ] || git apply < ../web.patch
 
 save() {
   ! git diff --exit-code || return 0
   git add -A
-  git reset --soft 8b23b3ff1879ca0442775a0ebf1defab72bbb79f
-  git commit -m "UPDATE"
-  git format-patch 8b23b3ff1879ca0442775a0ebf1defab72bbb79f --stdout > ../web.patch
+  git diff --cached > ../web.patch
 }
 
 ( while kill -0 $$ >/dev/null 2>&1; do save; sleep 1; done ) &
