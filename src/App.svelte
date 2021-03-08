4412ee89c829817f8a24afcf0ecde8f862044bfe
Diamond Cat <uyuuki0919@icloud.com>
2021-03-08 13:11:04 +0900

    commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,78 @@
 <script>
-	export let name;
+	import { onMount } from "svelte";
+
+	let data = [];
+	onMount(async () => {
+		data = await (await fetch("https://raw.githubusercontent.com/diamondcatpng/cross-internet/api/article.json")).json();
+	});
+
+	async function sleep(ms) {
+		await new Promise((x) => setTimeout(x, ms));
+	}
+
+	async function ogp(url) {
+		return await (await fetch("https://ricapitolare.vercel.app/?url=" + url)).json();
+	}
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
+	{#each data as content, i}
+		{#await sleep(i * 100) then _}
+			<a class="outline-none border hover:shadow-inner hover:bg-gray-100" href={content.url} target="_blank">
+				<div class="p-4 break-words">
+					{#await ogp(content.url)}
+						<p>読み込み中…</p>
+					{:then ogp}
+						<img class="w-full md:w-40 mb-2 md:mb-0 md:float-right md:rounded-3xl" src="https://images.weserv.nl/?url={ogp.image}&w=512&format=webp" alt={ogp.title} />
+						<h2 class="text-xl font-bold">{ogp.title}</h2>
+						<p class="text-gray-600">{ogp.description}</p>
+					{/await}
+				</div>
+			</a>
+		{/await}
+	{/each}
+</div>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<!-- <div
+	class="h-screen overflow-y-scroll"
+	on:mousewheel={() => console.log("a")}
+>
+		<div style="height: 1px;" />
+	<div class="h-screen bg-green-400" />
+		<div style="height: 1px;" />
+</div> -->
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<!-- <div class="grid md:grid-cols-2 xl:grid-cols-4">
+	{#each data as content}
+		{#await ogp(content.url)}
+			<p>読み込み中…</p>
+		{:then ogp}
+			<div class="overflow-hidden break-words">
+				<div class="m-4 border-2 rounded-lg hover:shadow-lg {content.live ? 'bg-red-300' : content.upcoming ? 'bg-green-300' : ''}">
+					<a href={content.url} target="_blank" class="outline-none">
+						<div class="p-4 flex justify-between">
+							<div class="flex items-center">
+								<img class="w-8" src="https://images.weserv.nl/?url={ogp.icon}&format=webp" alt={ogp.provider} />
+								<p class="ml-4">{ogp.provider}</p>
+							</div>
+							{#if content.live || content.upcoming}
+								<div class="flex items-center">
+									<span class="text-xl font-semibold {content.live ? 'text-red-600' : content.upcoming ? 'text-green-600' : ''}">{content.live ? "ライブ" : content.upcoming ? "待機枠" : ""}</span>
+								</div>
+							{/if}
+						</div>
+						{#if ogp.image}
+							<img class="w-full" src="https://images.weserv.nl/?url={ogp.image}&w=512&format=webp" alt={ogp.title} />
+						{/if}
+						<div class="px-6 py-4">
+							<div class="mb-2 font-bold text-xl">{ogp.title}</div>
+							{#if ogp.description}
+								<p class="text-base text-gray-700">{ogp.description}</p>
+							{/if}
+						</div>
+					</a>
+				</div>
+			</div>
+		{/await}
+	{/each}
+</div> -->
