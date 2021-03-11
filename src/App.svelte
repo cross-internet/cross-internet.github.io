4b360ce4ab03795ef22bd70a556144e872283dd9
Diamond Cat <uyuuki0919@icloud.com>
2021-03-11 15:43:52 +0900

    add alternative page

--- src/App.svelte
+++ src/App.svelte
@@ -13,6 +13,7 @@
 
 	let random = true;
 	$: data = random ? arrayShuffle(json) : json;
+	let alternative = false;
 
 	let buf = 20;
 	let now = 0;
@@ -31,13 +32,17 @@
 			<input type="checkbox" bind:checked={random} />
 			<span>ランダム</span>
 		</label>
+		<label>
+			<input type="checkbox" bind:checked={alternative} />
+			<span>代替ページ (β版)</span>
+		</label>
 	</div>
 
 	<div class="h-full overflow-y-scroll p-4 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5" on:scroll={(e) => (box = e.currentTarget)}>
 		{#each data as content, i}
 			<div class="break-words">
 				{#if i < (now + 1) * buf}
-					<Card url={content.url} traffic={content.traffic} date={content.date} />
+					<Card url={alternative && content.alternative_url ? content.alternative_url : content.url} traffic={content.traffic} date={content.date} />
 				{/if}
 			</div>
 		{/each}
